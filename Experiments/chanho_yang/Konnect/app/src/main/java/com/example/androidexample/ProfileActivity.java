689e4878edf98424b66eda17b3538af13de3b1b0
didcksgh <chanho.yang1226@gmail.com>
2024-02-02 15:04:46 -0600

    fix error and add scroll

--- Experiments/chanho_yang/Konnect/app/src/main/java/com/example/androidexample/ProfileActivity.java
+++ Experiments/chanho_yang/Konnect/app/src/main/java/com/example/androidexample/ProfileActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +17,7 @@ public class ProfileActivity extends AppCompatActivity {
     private EditText dob;
     private EditText hobby;
     private EditText address;
+    private Spinner gender;
     private Button submitButton;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +29,20 @@ public class ProfileActivity extends AppCompatActivity {
         dob = findViewById(R.id.profile_dob_edt);
         hobby = findViewById(R.id.profile_hobby_edt);
         address = findViewById(R.id.profile_address_edt);
+        gender = findViewById(R.id.profile_gender_spin);
         submitButton = findViewById(R.id.profile_submit_btn);
 
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.gender_array,  // Create a string array resource with gender options
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        gender.setAdapter(adapter);
+
         submitButton.setOnClickListener(new View.OnClickListener() {
+
+            String selectedGender = gender.getSelectedItem().toString();
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
