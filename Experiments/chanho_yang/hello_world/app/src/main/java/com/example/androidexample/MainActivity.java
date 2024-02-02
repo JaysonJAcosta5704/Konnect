be19f1053032bf4490c1f614a389b6f07a067166
didcksgh <chanho.yang1226@gmail.com>
2024-02-01 23:19:29 -0600

    add a text

--- Experiments/chanho_yang/hello_world/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/chanho_yang/hello_world/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -13,6 +13,7 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
 
     private TextView messageText;   // define message textview variable
+    private TextView messageText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
 
         /* initialize UI elements */
         messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
-        messageText.setText("Hello World");
+        messageText.setText("Hello Creatures");
+
+        messageText2 = findViewById(R.id.second_msg_txt);
+        messageText2.setText("Welcome!");
+
     }
 }
