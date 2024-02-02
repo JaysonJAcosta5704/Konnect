8fbf2d58dcd8d42323c1c8167c51d45464640e1c
didcksgh <chanho.yang1226@gmail.com>
2024-02-02 13:21:22 -0600

    add profile page

--- Experiments/chanho_yang/Konnect/app/src/main/java/com/example/androidexample/ProfileActivity.java
+++ Experiments/chanho_yang/Konnect/app/src/main/java/com/example/androidexample/ProfileActivity.java
@@ -1,4 +1,42 @@
 package com.example.androidexample;
 
-public class ProfileActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+    private EditText phoneNumber;
+    private EditText dob;
+    private EditText hobby;
+    private EditText address;
+
+    private Button homeButton;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);            // link to profile activity XML
+
+        /* initialize UI elements */
+        phoneNumber = findViewById(R.id.profile_phoneNumber_edt);
+        dob = findViewById(R.id.profile_dob_edt);
+        hobby = findViewById(R.id.profile_hobby_edt);
+        address = findViewById(R.id.profile_address_edt);
+        homeButton = findViewById(R.id.profile_home_btn);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
