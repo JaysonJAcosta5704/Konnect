8d565d29ec1e3e2fc10c25446bc048715f1312a6
Daniel Vergara <danielmauriciovpinilla@hotmail.com>
2024-02-01 20:00:33 -0600

    modifying hello world

--- Experiments/Daniel-Pinilla/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/Daniel-Pinilla/helloworld/src/main/java/coms309/WelcomeController.java
@@ -11,9 +11,28 @@ class WelcomeController {
     public String welcome() {
         return "Hello and welcome to COMS 309";
     }
-
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
-        return "Hello and welcome to COMS 309: " + name;
+        return "Hello and welcome toooo COMS 309: " + name;
     }
+
+    @GetMapping("/check-email/{enteredEmail}")
+    public String checkEmail(@PathVariable String enteredEmail) {
+
+        String test= "mitra@iastate.edu";
+        if (test.equals(enteredEmail)) {
+            return "Email is the default one.";
+        } else {
+            return "Email is not the default one.";
+        }
+    }
+
+    @GetMapping("/add/{num1}/{num2}")
+    public String addNumbers(@PathVariable int num1, @PathVariable int num2) {
+        int sum = num1 + num2;
+        return "The sum of " + num1 + " and " + num2 + " is: " + sum;
+    }
+
+
+
 }
