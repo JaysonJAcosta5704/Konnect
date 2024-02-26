be1fb55a5f77307211c499d3043e8e62f98e962b
Nishi Kant <nishikant@004029-monitor.lib.iastate.edu>
2024-02-26 16:33:27 -0600

    added hobbies controlelr and interface

--- Experiments/Daniel-Pinilla/backend_code/src/main/java/onetomany/Reports/ReportsRepository.java
+++
@@ -1,8 +0,0 @@
-package onetomany.Reports;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface ReportsRepository extends JpaRepository<Reports, Long>{
-    Reports findById(int id);
-
-
-}
