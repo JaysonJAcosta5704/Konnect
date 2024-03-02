5719bedcf9e332ba8c4a72bae87c683bab7ffef3
didcksgh <chanho.yang1226@gmail.com>
2024-03-02 11:27:45 -0600

    create hobby class

---
+++ Frontend/Konnect/app/src/main/java/com/example/konnect/Hobby.java
@@ -0,0 +1,21 @@
+package com.example.konnect;
+
+public class Hobby {
+    private String name;
+    private String type;
+
+    public Hobby(String name, String type){
+        this.name = name;
+        this.type = type;
+
+    }
+
+    public String getName(){
+        return name;
+
+    }
+
+    public String getType(){
+        return type;
+    }
+}
