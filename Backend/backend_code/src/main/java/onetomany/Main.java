cbef0ad7a52f0c8f0de424da553a23bbd3b70531
Nishi Kant <nishikant@Nishis-MacBook-Air.local>
2024-03-03 17:11:55 -0600

    added tables to workbench

--- Backend/backend_code/src/main/java/onetomany/Main.java
+++ Backend/backend_code/src/main/java/onetomany/Main.java
@@ -108,89 +108,104 @@ class Main {
     }
 
     // Create 3 users with their machines and phones
+//    @Bean
+//    CommandLineRunner initUser(UserRepository userRepository, MatchesRepository matchesRepository, ReportsRepository reportsRepository,HobbiesRepository hobbiesRepository) {
+//        return args -> {
+////
+//            User user1 = new User("Daniel", "dmvp01@somemail.com", new Date(), "Daniel123", "danielUser");
+//            User user2 = new User("Nishi", "Nishi@somemail.com", new Date(), "Nishi456", "nishiUser");
+//            User user3 = new User("Jayson", "Jayson@somemail.com", new Date(), "Jayson789", "jaysonUser");
+//
+////            Hobbies painting = new Hobbies("Painting", HobbyType.INDOOR);
+////            Hobbies football = new Hobbies("Football", HobbyType.OUTDOOR);
+////            Hobbies bookClub = new Hobbies("Book Club", HobbyType.GROUP);
+////
+////            hobbiesRepository.save(painting);
+////            hobbiesRepository.save(football);
+////            hobbiesRepository.save(bookClub);
+////            User updatedUser1 = userRepository.findById(user1.getId());
+////            User updatedUser2 = userRepository.findById(user2.getId());
+////            User updatedUser3 = userRepository.findById(user3.getId());
+////
+////
+////
+////            userRepository.save(user1);
+////            userRepository.save(user2);
+////            userRepository.save(user3);
+//
+//
+//
+//            user1 = userRepository.save(user1);
+//            user2 = userRepository.save(user2);
+//            user3 = userRepository.save(user3);
+//
+//            // Create and save hobbies
+//            Hobbies painting = hobbiesRepository.save(new Hobbies("Painting", HobbyType.INDOOR));
+//            Hobbies football = hobbiesRepository.save(new Hobbies("Football", HobbyType.OUTDOOR));
+//            Hobbies bookClub = hobbiesRepository.save(new Hobbies("Book Club", HobbyType.GROUP));
+//
+//            // Associate hobbies with users
+//            user1.getHobbies().add(painting); // Daniel likes Painting
+//            user2.getHobbies().add(football); // Nishi likes Football
+//            user3.getHobbies().add(bookClub); // Jayson likes Book Club
+//
+//            // Save the users again to update the relationship
+//            userRepository.save(user1);
+//            userRepository.save(user2);
+//            userRepository.save(user3);
+//
+//            reportsRepository.save(new Reports("Bad photo"));
+//            reportsRepository.save(new Reports("Weird Bio"));
+//            reportsRepository.save(new Reports("Agressive"));
+//            reportsRepository.save(new Reports("Rude"));
+//            Reports r1 = reportsRepository.findById(1);
+//            Reports r2 = reportsRepository.findById(2);
+//            Reports r3 = reportsRepository.findById(3);
+//            Reports r4= reportsRepository.findById(4);
+//
+//
+//
+//
+//            r1.setUser(user1);
+//            r2.setUser(user1);
+//            r3.setUser(user2);
+//            r4.setUser(user3);
+//
+//            reportsRepository.save(r1);
+//            reportsRepository.save(r2);
+//            reportsRepository.save(r3);
+//            reportsRepository.save(r4);
+//            user1.addReport(r1);
+//            user1.addReport(r2);
+//            user2.addReport(r3);
+//            user3.addReport(r4);
+//
+//
+//            System.out.println(user1.getReports());
+//
+//
+//
+//
+//
+//
+////            userRepository.save(user1);
+////            userRepository.save(user2);
+////            userRepository.save(user3);
+//        };
+//    }
     @Bean
-    CommandLineRunner initUser(UserRepository userRepository, MatchesRepository matchesRepository, ReportsRepository reportsRepository,HobbiesRepository hobbiesRepository) {
-        return args -> {
-//
-            User user1 = new User("Daniel", "dmvp01@somemail.com", new Date(), "Daniel123", "danielUser");
-            User user2 = new User("Nishi", "Nishi@somemail.com", new Date(), "Nishi456", "nishiUser");
-            User user3 = new User("Jayson", "Jayson@somemail.com", new Date(), "Jayson789", "jaysonUser");
+    public CommandLineRunner demo(UserRepository userRepository) {
+        return (args) -> {
+            // Create a new user and save it in the database
+            User newUser = new User("Test User", "testuser@example.com", new Date(), "TestUserPassword", "testUsername");
+            userRepository.save(newUser);
 
-//            Hobbies painting = new Hobbies("Painting", HobbyType.INDOOR);
-//            Hobbies football = new Hobbies("Football", HobbyType.OUTDOOR);
-//            Hobbies bookClub = new Hobbies("Book Club", HobbyType.GROUP);
-//
-//            hobbiesRepository.save(painting);
-//            hobbiesRepository.save(football);
-//            hobbiesRepository.save(bookClub);
-//            User updatedUser1 = userRepository.findById(user1.getId());
-//            User updatedUser2 = userRepository.findById(user2.getId());
-//            User updatedUser3 = userRepository.findById(user3.getId());
-//
-//
-//
-//            userRepository.save(user1);
-//            userRepository.save(user2);
-//            userRepository.save(user3);
-
-
-
-            user1 = userRepository.save(user1);
-            user2 = userRepository.save(user2);
-            user3 = userRepository.save(user3);
-
-            // Create and save hobbies
-            Hobbies painting = hobbiesRepository.save(new Hobbies("Painting", HobbyType.INDOOR));
-            Hobbies football = hobbiesRepository.save(new Hobbies("Football", HobbyType.OUTDOOR));
-            Hobbies bookClub = hobbiesRepository.save(new Hobbies("Book Club", HobbyType.GROUP));
-
-            // Associate hobbies with users
-            user1.getHobbies().add(painting); // Daniel likes Painting
-            user2.getHobbies().add(football); // Nishi likes Football
-            user3.getHobbies().add(bookClub); // Jayson likes Book Club
-
-            // Save the users again to update the relationship
-            userRepository.save(user1);
-            userRepository.save(user2);
-            userRepository.save(user3);
-
-            reportsRepository.save(new Reports("Bad photo"));
-            reportsRepository.save(new Reports("Weird Bio"));
-            reportsRepository.save(new Reports("Agressive"));
-            reportsRepository.save(new Reports("Rude"));
-            Reports r1 = reportsRepository.findById(1);
-            Reports r2 = reportsRepository.findById(2);
-            Reports r3 = reportsRepository.findById(3);
-            Reports r4= reportsRepository.findById(4);
-
-
-
-
-            r1.setUser(user1);
-            r2.setUser(user1);
-            r3.setUser(user2);
-            r4.setUser(user3);
-
-            reportsRepository.save(r1);
-            reportsRepository.save(r2);
-            reportsRepository.save(r3);
-            reportsRepository.save(r4);
-            user1.addReport(r1);
-            user1.addReport(r2);
-            user2.addReport(r3);
-            user3.addReport(r4);
-
-
-            System.out.println(user1.getReports());
-
-
-
-
-
-
-//            userRepository.save(user1);
-//            userRepository.save(user2);
-//            userRepository.save(user3);
+            // List all users
+            System.out.println("Users found with findAll():");
+            System.out.println("-------------------------------");
+            for (User user : userRepository.findAll()) {
+                System.out.println(user.getName());
+            }
         };
     }
 
--- Backend/backend_code/src/main/java/onetomany/Matches/Match.java
+++ Backend/backend_code/src/main/java/onetomany/Matches/Match.java
@@ -1,15 +1,11 @@
 package onetomany.Matches;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import onetomany.Users.User;
 @Entity
+@Table(name="matches")
 public class Match {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
