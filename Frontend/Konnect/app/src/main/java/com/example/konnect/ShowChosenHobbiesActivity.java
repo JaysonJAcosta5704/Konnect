997bd5d21ae36cebf491b58300c2a968a6cca01e
didcksgh <chanho.yang1226@gmail.com>
2024-03-03 08:52:52 -0600

    fix error to get data

--- Frontend/Konnect/app/src/main/java/com/example/konnect/ShowChosenHobbiesActivity.java
+++ Frontend/Konnect/app/src/main/java/com/example/konnect/ShowChosenHobbiesActivity.java
@@ -30,7 +30,8 @@ public class ShowChosenHobbiesActivity extends AppCompatActivity {
 
     private LinearLayout hobbiesLayout;
 
-    public void onCreate(Bundle savedInstanceState){
+
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_displayhobby);
 
