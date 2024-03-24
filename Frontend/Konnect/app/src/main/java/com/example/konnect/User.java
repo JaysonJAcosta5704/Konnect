a95f94ad8d8488f1a25151de5f0998738f0adfb7
Jayson Acosta <JaysonJAcosta5704.code@gmail.com>
2024-03-24 01:38:33 -0500

    Added methods to set and get URL for userinformation, to be used by ProfileActivity

--- Frontend/Konnect/app/src/main/java/com/example/konnect/User.java
+++ Frontend/Konnect/app/src/main/java/com/example/konnect/User.java
@@ -3,16 +3,12 @@ package com.example.konnect;
 import android.content.Context;
 import android.widget.Toast;
 
-import java.util.Date;
-
 public class User {
-    private String name, email, username, gender, bio, password, ID;
-    private int age;
-    private Date birthday;
+    private String name, email, username, gender, bio, password, ID, birthday, age;
     private static User instance;
 //    private final String SERVER_URL = "http://coms-309-001.class.las.iastate.edu:8080/";
     private final String SERVER_URL = "https://df952b3b-a205-4a2f-a0e0-a0f471c5f2bb.mock.pstmn.io/";
-    private String USER_URL;
+    private String USERLOGIN_URL, USERINFO_URL;
     /* Constructor */
     public User(){}
 
@@ -23,31 +19,34 @@ public class User {
     public void setPassword(String password) { this.password = password; }
 
     public void setName(String name) { this.name = name; }
-    public void setAge(int age) { this.age = age;}
+    public void setAge(String age) { this.age = age;}
     public void setGender(String gender) { this.gender = gender; }
 
     public void setBio(String bio) { this.bio = bio; }
 
-    public void setBirthday(Date birthday) { this.birthday = birthday; }
+    public void setBirthday(String birthday) { this.birthday = birthday; }
 
     /* Getter Methods */
     public String getUsername() { return username; }
     public String getEmail() { return email; }
     public String getID() { return ID; }
     public String getName() { return name; }
-    public int getAge() { return age; }
+    public String getAge() { return age; }
     public String getGender() { return gender; }
     public String getBio() { return bio; }
-    public Date getBirthday() { return birthday; }
+    public String getBirthday() { return birthday; }
     public static synchronized User getInstance() {
         if (instance == null){ instance = new User(); }
         return instance;
     }
 
     /* URLS METHODS */
-    public void setURL_UP(){ USER_URL = SERVER_URL + "user/" + username + "/" + password; }
-    public void setURL_EP(){ USER_URL = SERVER_URL + "user/" + email + "/" + password; }
-    public String getURL(){ return USER_URL; }
+    public void setURL_UP(){ USERLOGIN_URL = SERVER_URL + "user/" + username + "/" + password; }
+    public void setURL_EP(){ USERLOGIN_URL = SERVER_URL + "user/" + email + "/" + password; }
+    public String getURL_USERLOGIN(){ return USERLOGIN_URL; }
+
+    public void setURL_USERINFO(){ USERINFO_URL = SERVER_URL + "user/" + ID; }
+    public String getURL_USERINFO(){ return USERINFO_URL; }
 
     /* Other Methods */
     public static void toastError(Context context, int errorCode){
@@ -58,10 +57,7 @@ public class User {
                 text = "There was an error while attempting to login";
                 break;
             case 1:
-                text = "There is no user with your username.";
-                break;
-            case 2:
-                text = "Your password is incorrect.";
+                text = "There is an error with the server";
                 break;
             default:
                 text = "Something went wrong";
