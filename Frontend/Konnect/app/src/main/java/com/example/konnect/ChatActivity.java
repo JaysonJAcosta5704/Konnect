622815e6a55a021fffa5ba54cd95c99d1d8c2f25
Jayson Acosta <JaysonJAcosta5704.code@gmail.com>
2024-03-26 10:32:43 -0500

    ChatActivity working, needs to be made into single user to user DMS

--- Frontend/Konnect/app/src/main/java/com/example/konnect/ChatActivity.java
+++ Frontend/Konnect/app/src/main/java/com/example/konnect/ChatActivity.java
@@ -1,14 +1,63 @@
 package com.example.konnect;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ChatActivity extends AppCompatActivity {
+import org.java_websocket.handshake.ServerHandshake;
+
+public class ChatActivity extends AppCompatActivity implements WebSocketListener{
+
+    TextView chatReceive;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        Button homeButton = findViewById(R.id.Chat_Home_Button);
+        homeButton.setOnClickListener(v -> startActivity(new Intent(v.getContext(), SettingsActivity.class)));
+
+        Button sendButton = findViewById(R.id.Chat_Send_Button);
+
+        EditText sendEditText = findViewById(R.id.Chat_Send_EditText);
+        chatReceive = findViewById(R.id.Chat_Receive);
+
+        sendButton.setOnClickListener(v -> {
+            try { WebSocketManager.getInstance().sendMessage(sendEditText.getText().toString()); }
+            catch (Exception e) { Log.d("ExceptionSendMessage:", e.toString()); }
+        });
+
+        String wsURL = "ws://10.0.2.2:8080/chat/" + User.getInstance().getUsername();
+        WebSocketManager.getInstance().connectWebSocket(wsURL);
+        WebSocketManager.getInstance().setWebSocketListener(this);
     }
+
+    @Override
+    public void onWebSocketOpen(ServerHandshake handshakedata) {}
+
+    @Override
+    public void onWebSocketMessage(String message) {
+        runOnUiThread(() -> {
+            String s = chatReceive.getText().toString();
+            chatReceive.setText(String.format("%s\n%s", s, message));
+        });
+    }
+
+    @Override
+    public void onWebSocketClose(int code, String reason, boolean remote) {
+        String closedBy = remote ? "server" : "local";
+        runOnUiThread(() -> {
+            String s = chatReceive.getText().toString();
+            chatReceive.setText(String.format("%s---\nconnection closed by %s\nreason: %s", s, closedBy, reason));
+        });
+    }
+
+    @Override
+    public void onWebSocketError(Exception ex) { Log.e("Websocket Error", ex.toString()); }
 }
