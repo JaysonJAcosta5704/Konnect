ed21239277ded46a1e060f64ce2079315b748d50
Jayson Acosta <JaysonJAcosta5704.code@gmail.com>
2024-04-01 16:04:27 -0500

    changed access modifiers to private and protected

--- Frontend/Konnect/app/src/main/java/com/example/konnect/User.java
+++ Frontend/Konnect/app/src/main/java/com/example/konnect/User.java
@@ -80,7 +80,7 @@ public class User {
     /**
      * Default constructor for the User class.
      */
-    public User(){}
+    private User(){}
 
     /* Setter Methods */
 
@@ -89,63 +89,63 @@ public class User {
      *
      * @param username The username to be set.
      */
-    public void setUsername(String username) { this.username = username; }
+    protected void setUsername(String username) { this.username = username; }
 
     /**
      * Sets the email of the user.
      *
      * @param email The email to be set.
      */
-    public void setEmail(String email) { this.email = email; }
+    protected void setEmail(String email) { this.email = email; }
 
     /**
      * Sets the ID of the user.
      *
      * @param ID The ID to be set.
      */
-    public void setID(String ID) { this.ID = ID;}
+    protected void setID(String ID) { this.ID = ID;}
 
     /**
      * Sets the password of the user.
      *
      * @param password The password to be set.
      */
-    public void setPassword(String password) { this.password = password; }
+    protected void setPassword(String password) { this.password = password; }
 
     /**
      * Sets the name of the user.
      *
      * @param name The name to be set.
      */
-    public void setName(String name) { this.name = name; }
+    protected void setName(String name) { this.name = name; }
 
     /**
      * Sets the age of the user.
      *
      * @param age The age to be set.
      */
-    public void setAge(String age) { this.age = age;}
+    protected void setAge(String age) { this.age = age;}
 
     /**
      * Sets the gender of the user.
      *
      * @param gender The gender to be set.
      */
-    public void setGender(String gender) { this.gender = gender; }
+    protected void setGender(String gender) { this.gender = gender; }
 
     /**
      * Sets the bio of the user.
      *
      * @param bio The bio to be set.
      */
-    public void setBio(String bio) { this.bio = bio; }
+    protected void setBio(String bio) { this.bio = bio; }
 
     /**
      * Sets the birthday of the user.
      *
      * @param birthday The birthday to be set.
      */
-    public void setBirthday(String birthday) { this.birthday = birthday; }
+    protected void setBirthday(String birthday) { this.birthday = birthday; }
 
     /* Getter Methods */
     /**
@@ -153,63 +153,63 @@ public class User {
      *
      * @return The username of the user.
      */
-    public String getUsername() { return username; }
+    protected String getUsername() { return username; }
 
     /**
      * Gets the email of the user.
      *
      * @return The email of the user.
      */
-    public String getEmail() { return email; }
+    protected String getEmail() { return email; }
 
     /**
      * Gets the ID of the user.
      *
      * @return The ID of the user.
      */
-    public String getID() { return ID; }
+    protected String getID() { return ID; }
 
     /**
      * Gets the name of the user.
      *
      * @return The name of the user.
      */
-    public String getName() { return name; }
+    protected String getName() { return name; }
 
     /**
      * Gets the age of the user.
      *
      * @return The age of the user.
      */
-    public String getAge() { return age; }
+    protected String getAge() { return age; }
 
     /**
      * Gets the gender of the user.
      *
      * @return The gender of the user.
      */
-    public String getGender() { return gender; }
+    protected String getGender() { return gender; }
 
     /**
      * Gets the bio of the user.
      *
      * @return The bio of the user.
      */
-    public String getBio() { return bio; }
+    protected String getBio() { return bio; }
 
     /**
      * Gets the birthday of the user.
      *
      * @return The birthday of the user.
      */
-    public String getBirthday() { return birthday; }
+    protected String getBirthday() { return birthday; }
 
     /**
      * Gets the singleton instance of the User class.
      *
      * @return The instance of the User class.
      */
-    public static synchronized User getInstance() {
+    protected static synchronized User getInstance() {
         if (instance == null){ instance = new User(); }
         return instance;
     }
@@ -218,31 +218,31 @@ public class User {
     /**
      * Sets the URL for user login with the provided username and password.
      */
-    public void setURL_UP(){ USERLOGIN_URL = SERVER_URL + "user/" + username + "/" + password; }
+    protected void setURL_UP(){ USERLOGIN_URL = SERVER_URL + "user/" + username + "/" + password; }
 
     /**
      * Sets the URL for user login with the provided email and password.
      */
-    public void setURL_EP(){ USERLOGIN_URL = SERVER_URL + "user/" + email + "/" + password; }
+    protected void setURL_EP(){ USERLOGIN_URL = SERVER_URL + "user/" + email + "/" + password; }
 
     /**
      * Gets the URL for user login.
      *
      * @return The URL for user login.
      */
-    public String getURL_USERLOGIN(){ return USERLOGIN_URL; }
+    protected String getURL_USERLOGIN(){ return USERLOGIN_URL; }
 
     /**
      * Sets the URL for user information with the provided ID.
      */
-    public void setURL_USERINFO(){ USERINFO_URL = SERVER_URL + "user/" + ID; }
+    protected void setURL_USERINFO(){ USERINFO_URL = SERVER_URL + "user/" + ID; }
 
     /**
      * Gets the URL for user information.
      *
      * @return The URL for user information.
      */
-    public String getURL_USERINFO(){ return USERINFO_URL; }
+    protected String getURL_USERINFO(){ return USERINFO_URL; }
 
     /* Other Methods */
     /**
@@ -253,7 +253,7 @@ public class User {
      * @param error     The error message to be logged.
      */
 
-    public static void toastError(Context context, int errorCode, String error){
+    protected static void toastError(Context context, int errorCode, String error){
         int duration = Toast.LENGTH_LONG;
         String text;
         switch (errorCode){
