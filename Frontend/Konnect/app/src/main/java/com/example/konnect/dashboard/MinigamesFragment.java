4590500400ccf50cd8d47cdae43f1a5db1385b0a
Jayson Acosta <JaysonJAcosta5704.code@gmail.com>
2024-04-20 05:50:49 -0500

    Created MinigamesFragment

---
+++ Frontend/Konnect/app/src/main/java/com/example/konnect/dashboard/MinigamesFragment.java
@@ -0,0 +1,22 @@
+package com.example.konnect.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.konnect.R;
+
+public class MinigamesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_settings, container, false);
+
+
+
+        return view;
+    }
+}
