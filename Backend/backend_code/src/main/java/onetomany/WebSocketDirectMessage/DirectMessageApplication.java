799cc5d5aafa21be9f67d4636a1b2a632262d4df
Nishi Kant <nishikant@Nishis-MacBook-Air.local>
2024-04-30 07:50:19 -0500

    websocket for dm

---
+++ Backend/backend_code/src/main/java/onetomany/WebSocketDirectMessage/DirectMessageApplication.java
@@ -0,0 +1,11 @@
+package onetomany.WebSocketDirectMessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectMessageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DirectMessageApplication.class, args);
+    }
+}
