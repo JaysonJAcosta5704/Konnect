d3427f4bccd7a036cdd02851b876f6caaba64787
didcksgh <chanho.yang1226@gmail.com>
2024-04-29 23:43:40 -0500

    implement adminuser login and signup activities

---
+++ Frontend/Konnect/app/src/main/java/com/example/konnect/AdminLoginActivity.java
@@ -0,0 +1,6 @@
+package com.example.konnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminLoginActivity extends AppCompatActivity {
+}
---
+++ Frontend/Konnect/app/src/main/java/com/example/konnect/AdminSignupActivity.java
@@ -0,0 +1,6 @@
+package com.example.konnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminSignupActivity extends AppCompatActivity {
+}
