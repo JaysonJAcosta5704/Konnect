8076ab5aaee7e305513a4d43f47db7b4e945d907
Jayson Acosta <JaysonJAcosta5704.code@gmail.com>
2024-04-30 17:31:32 -0500

    Fixed signup

--- Frontend/Konnect/app/src/main/java/com/example/konnect/entry/SignupActivity.java
+++ Frontend/Konnect/app/src/main/java/com/example/konnect/entry/SignupActivity.java
@@ -3,23 +3,16 @@ package com.example.konnect.entry;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.konnect.SettingsActivity;
 import com.example.konnect.R;
 
 import org.json.JSONException;
@@ -94,163 +87,131 @@ public class SignupActivity extends AppCompatActivity {
         });
 **/
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        loginButton.setOnClickListener(v -> {
 
-                /* when login button is pressed, use intent to switch to Login Activity */
-                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
-                startActivity(intent);  // go to LoginActivity
-            }
+            /* when login button is pressed, use intent to switch to Login Activity */
+            Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+            startActivity(intent);  // go to LoginActivity
         });
 
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        signupButton.setOnClickListener(v -> {
 
-                /* grab strings from user inputs */
+            /* grab strings from user inputs */
 
-                String username = usernameEditText.getText().toString();
-                String name = nameEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
-                String confirm = confirmEditText.getText().toString();
-                String email = emailAccountEditText.getText().toString();
-                String ageString = ageEditText.getText().toString();
-                String dobString = dobEditText.getText().toString();
-                String gender = genderEditText.getText().toString();
+            String username = usernameEditText.getText().toString();
+            String name = nameEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+            String confirm = confirmEditText.getText().toString();
+            String email = emailAccountEditText.getText().toString();
+            String ageString = ageEditText.getText().toString();
+            String dobString = dobEditText.getText().toString();
+            String gender = genderEditText.getText().toString();
 
-                Log.d("Debug", "Username: " + username);
+            Log.d("Debug", "Username: " + username);
 
-                int age = Integer.parseInt(ageString);
+            int age = Integer.parseInt(ageString);
 
-                //check if user didn't left the blank
-                if(username.isEmpty()){
-                    Toast.makeText(SignupActivity.this, "Please, provide your username", Toast.LENGTH_SHORT).show();
+            //check if user didn't left the blank
+            if(username.isEmpty()){
+                Toast.makeText(SignupActivity.this, "Please, provide your username", Toast.LENGTH_SHORT).show();
+                return;
+
+            }else if(email.isEmpty()){
+                Toast.makeText(SignupActivity.this, "Please, provide your email.", Toast.LENGTH_SHORT).show();
+                return;
+
+            }else if(password.isEmpty()){
+                Toast.makeText(SignupActivity.this, "Please, provide your password.", Toast.LENGTH_SHORT).show();
+                return;
+
+            }else if(confirm.isEmpty()){
+                Toast.makeText(SignupActivity.this, "Please, provide your confirm password.", Toast.LENGTH_SHORT).show();
+                return;
+
+            }else if(dobString.isEmpty()){
+                Toast.makeText(SignupActivity.this, "Please, provide your date of birth.", Toast.LENGTH_SHORT).show();
+                return;
+
+            }else if(gender.isEmpty()){
+                Toast.makeText(SignupActivity.this, "Please, provide your gender.", Toast.LENGTH_SHORT).show();
+                return;
+
+            }else if(ageEditText.getText().toString().isEmpty()){
+                Toast.makeText(SignupActivity.this, "Please, provide your age.", Toast.LENGTH_SHORT).show();
+                return;
+
+            }
+
+            /**SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+            String currentDate = df.format(new Date());**/
+
+            if (password.equals(confirm)){
+                Toast.makeText(getApplicationContext(), "Signing up", Toast.LENGTH_LONG).show();
+
+                // Parse the date string into a Date object
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                Date dob;
+                try {
+                    dob = df.parse(dobString);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                    Toast.makeText(SignupActivity.this, "Please, provide the correct form of DOB", Toast.LENGTH_SHORT).show();
                     return;
-
-                }else if(email.isEmpty()){
-                    Toast.makeText(SignupActivity.this, "Please, provide your email.", Toast.LENGTH_SHORT).show();
-                    return;
-
-                }else if(password.isEmpty()){
-                    Toast.makeText(SignupActivity.this, "Please, provide your password.", Toast.LENGTH_SHORT).show();
-                    return;
-
-                }else if(confirm.isEmpty()){
-                    Toast.makeText(SignupActivity.this, "Please, provide your confirm password.", Toast.LENGTH_SHORT).show();
-                    return;
-
-                }else if(dobString.isEmpty()){
-                    Toast.makeText(SignupActivity.this, "Please, provide your date of birth.", Toast.LENGTH_SHORT).show();
-                    return;
-
-                }else if(gender.isEmpty()){
-                    Toast.makeText(SignupActivity.this, "Please, provide your gender.", Toast.LENGTH_SHORT).show();
-                    return;
-
-                }else if(ageEditText.getText().toString().isEmpty()){
-                    Toast.makeText(SignupActivity.this, "Please, provide your age.", Toast.LENGTH_SHORT).show();
-                    return;
-
                 }
 
-                /**SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-                String currentDate = df.format(new Date());**/
-
-                if (password.equals(confirm)){
-                    Toast.makeText(getApplicationContext(), "Signing up", Toast.LENGTH_LONG).show();
-
-                    // Parse the date string into a Date object
-                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-                    Date dob;
-                    try {
-                        dob = df.parse(dobString);
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                        Toast.makeText(SignupActivity.this, "Please, provide the correct form of DOB", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-
-                    // Format the Date object into an ISO 8601 string
-                    df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX", Locale.getDefault());
-                    String dobISO = df.format(dob);
+                // Format the Date object into an ISO 8601 string
+                df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX", Locale.getDefault());
+                String dobISO = df.format(dob);
 
 
-                    String url = "http://coms-309-001.class.las.iastate.edu:8080/users/";
-                    JSONObject params = new JSONObject();
-                    try {
-                        params.put("name", name);
-                        params.put("emailId", email);
-                        params.put("userPassword", password);
-                        params.put("userName", username);
-                        params.put("age", age);
-                        params.put("birthday", dobISO);
-                        params.put("gender", gender);
+                String url = "http://10.0.2.2:8080/users/";
+                JSONObject params = new JSONObject();
+                try {
+                    params.put("name", name);
+                    params.put("emailId", email);
+                    params.put("userPassword", password);
+                    params.put("username", username);
+                    params.put("age", age);
+                    params.put("birthday", dobISO);
+                    params.put("gender", gender);
 
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                        Log.e("Error Json",e.toString());
-                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.e("Error Json",e.toString());
+                }
 
-                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
 
-                            (Request.Method.POST, url, params, new Response.Listener<JSONObject>() {
+                        (Request.Method.POST, url, params, response -> {
+                            try {
+                                String status = response.getString("message"); // Get the status from the response
 
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                    try {
-                                        String status = response.getString("message"); // Get the status from the response
-
-                                        if (status.equals("success")) {
-                                            // Display success message
-                                            Toast.makeText(SignupActivity.this, "Signup successful!", Toast.LENGTH_SHORT).show();
-
-                                            // Start SettingsActivity
-                                            Intent intent = new Intent(SignupActivity.this, SettingsActivity.class);
-                                            startActivity(intent);
-                                        } else {
-                                            // Display failure message
-                                            Toast.makeText(SignupActivity.this, "Signup failed!", Toast.LENGTH_SHORT).show();
-                                        }
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
+                                if (status.equals("success")) {
+                                    Toast.makeText(SignupActivity.this, "Signup successful!", Toast.LENGTH_SHORT).show();
+                                    finish();
                                 }
-
-                            }, new Response.ErrorListener() {
-
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    //Toast.makeText(SignupActivity.this, "Error", Toast.LENGTH_LONG).show();
-
-                                    // Check if the error is a network error
-                                    if (error instanceof com.android.volley.NoConnectionError) {
-                                        Toast.makeText(SignupActivity.this, "Network error", Toast.LENGTH_LONG).show();
-                                    } else {
-                                        Toast.makeText(SignupActivity.this, "Error", Toast.LENGTH_LONG).show();
-                                    }
-                                }
-                            });
+                                else { Toast.makeText(SignupActivity.this, "Signup failed!", Toast.LENGTH_SHORT).show(); }
+                            } catch (JSONException e) { Log.e("Error", e.toString()); }
+                        }, error -> {
+                            if (error instanceof com.android.volley.NoConnectionError) { Toast.makeText(SignupActivity.this, "Network error", Toast.LENGTH_LONG).show(); }
+                            else { Toast.makeText(SignupActivity.this, "Error", Toast.LENGTH_LONG).show(); }
+                        });
 
 
 
-                    RequestQueue queue = Volley.newRequestQueue(SignupActivity.this);
-                    queue.add(jsonObjectRequest);
+                RequestQueue queue = Volley.newRequestQueue(SignupActivity.this);
+                queue.add(jsonObjectRequest);
 
-                }
-                else {
-                    Toast.makeText(getApplicationContext(), "Password don't match", Toast.LENGTH_LONG).show();
-                }
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "Password don't match", Toast.LENGTH_LONG).show();
             }
         });
 
 
-        homeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
+        homeButton.setOnClickListener(view -> {
+            Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+            startActivity(intent);
         });
     }
 }
