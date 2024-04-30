f604acdd3d7c004cc39c7ff199de44cbf4552a90
didcksgh <chanho.yang1226@gmail.com>
2024-04-30 13:09:33 -0500

    implement findalluser

--- Frontend/Konnect/app/src/main/java/com/example/konnect/AdminFindAllActivity.java
+++ Frontend/Konnect/app/src/main/java/com/example/konnect/AdminFindAllActivity.java
@@ -1,6 +1,64 @@
 package com.example.konnect;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
+import java.util.List;
+
+import com.example.konnect.helper.OtherUser;
 
 public class AdminFindAllActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<OtherUser> userList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_find_all);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://coms-309-001.class.las.iastate.edu:8080/adminUser/getUsers/email/password/";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Gson gson = new Gson();
+                        Type listType = new TypeToken<List<OtherUser>>() {}.getType();
+                        userList = gson.fromJson(response.toString(), listType);
+                        recyclerView.setAdapter(new UserAdapter(userList));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Handle error
+            }
+        });
+
+        queue.add(jsonArrayRequest);
+    }
+
 }
